d74c5105c8de1115f3ae5c78207a0ddcb1d575cf
wodewone <wodewone@sina.com>
2019-12-09 14:50:50 +0800

    mod pm2 config file.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,9 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    min_uptime: 10000,
+    max_restarts: 5,
+    max_memory_restart: '90M',
     env: {
       NODE_ENV: 'development'
     },
