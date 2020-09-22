058512f2e564553dcfb4a02cd8da31754766014b
wodewone <wodewone@sina.com>
2020-09-22 09:58:17 +0800

    add router for otc

--- router/index.js
+++ router/index.js
@@ -5,12 +5,16 @@ const router = new Router({
     prefix: '/v1'
 });
 
-const db_hb = new Database({db: 'hb'});
-
 router.get('/hb/volume', async ctx => {
+    const db_hb = new Database({db: 'hb'});
     const {query: {limit} = {}} = ctx;
     ctx.body = await db_hb.queryData(limit);
+});
 
+router.get('/hb/otc', async ctx => {
+    const db_hb = new Database({db: 'hbOtc'});
+    const {query: {limit} = {}} = ctx;
+    ctx.body = await db_hb.queryData(limit);
 });
 
 module.exports = router;
